374a1feee157ed34dfcf93c2bd135687bffada92
Winifred <winifredasante15@gmail.com>
2022-09-25 01:35:52 -0400

    commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const starsEl  = document.querySelectorAll(".fa-star");
+const emojisEl  = document.querySelectorAll(".far");
+
+const colors = ["red","orange","lightblue","lightgreen","green"];
+
+updateRating(0);
+
+starsEl.forEach((starsEl,index)=>{
+    starsEl.addEventListener("click",() => {
+        updateRating(index);
+    })
+})
+
+
+function updateRating(index){
+    starsEl.forEach((starsEl,idx)=>{
+        if(idx<index + 1)
+     {
+        starsEl.classList.add("active");
+     }else{
+         starsEl.classList.remove("active")
+     }
+    })
+   
+
+    emojisEl.forEach(emojisEl=>{
+        emojisEl.style.transform = 
+        `translateX(-${index * 50}px)`
+        emojisEl.style.color = colors[index]
+    })
+}
